0a636e42b3c025aeb81be1d11c096f11bfc60fdb
Otha D Hernandez <otha.hernandez@gmail.com>
2019-05-01 03:12:11 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {} # Creates a blank hash to store new hash
+  languages.each do |style, keys| # iterates through first layer
+    keys.each do |language, type| # Iterates through second layer
+    new_hash[language] = type # Creates hash key val type
+    if new_hash[language][:style].length > 0
+      new_hash[language][:style] << style
+    else
+      new_hash[language][:style] = []
+    end
+  end
+  end
+  
+  new_hash
 end
